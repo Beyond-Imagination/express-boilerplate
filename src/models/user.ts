5d355686109d33293e94ff982331cf49fa654461
laggu <sebastian.ra@groundx.xyz>
2021-11-28 20:35:34 +0900

    add user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,18 @@
-import { model } from 'dynamoose'
-import { DocumentModel } from '@/types/document-model'
-import { UserSchema } from '@/schema/user'
+import { Schema, model } from 'mongoose'
 
-export default model<DocumentModel.User>('byd.example.user', UserSchema)
+export interface IUser {
+  email: string
+  password: string
+  nickname: string
+  type: number // local, kakao, google, apple
+  // 여러 oauth 들을 어떻게 다룰지 고민
+}
+
+const schema = new Schema<IUser>({
+  email: { type: String, required: true },
+  password: { type: String },
+  nickname: { type: String, required: true },
+  type: { type: Number, required: true },
+})
+
+export const User = model<IUser>('User', schema)
