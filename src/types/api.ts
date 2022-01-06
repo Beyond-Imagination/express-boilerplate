49cd80f17c8f052f63eaada03dd27fe988cbeecb
laggu <sebastian.ra@groundx.xyz>
2022-01-07 00:20:54 +0900

    add error class

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,29 @@
-export declare namespace API {
+export namespace API {
   export interface Response {
     message: string | null
     success: boolean
     status: number
     result: any
+  }
+
+  export class APIError extends Error {
+    status: number
+    code: number
+    message: string
+
+    constructor (statusCode: number, errorCode: number, message: string) {
+      super(message)
+
+      this.status = statusCode
+      this.code = errorCode
+      this.message = message
+    }
+
+    toJSON(){
+      return {
+        code: this.code,
+        message: this.message,
+      }
+    }
   }
 }
