8024176b8ff9ace6abef54e73f14f65a52d5a188
Inho Baek <inho.baek@nhn.com>
2022-01-12 03:34:07 +0900

    chore: add node.js CORS middleware
    issue: resolved #20

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,34 +1,44 @@
 import express from 'express'
 import session from 'express-session'
 import passport from 'passport'
+import cors from 'cors'
 
 import routes from '@/routes'
-import {errorHandler} from '@/middlewares'
-import {morganLogger} from '@/libs'
-import {env} from '@/env'
+import { errorHandler } from '@/middlewares'
+import { morganLogger } from '@/libs'
+import { env } from '@/env'
 
-const {sessionKey} = env
+const { sessionKey } = env
 
 export const expressLoader = () => {
   const app = express()
 
-  app.use(express.json())
-  app.use(session({
-    secret: sessionKey,
-    resave: false,
-    saveUninitialized: false
-  }))
+  // Header: cors
+  app.use(cors())
 
-  //  Passport
+  // Body: json
+  app.use(express.json())
+
+  // Authentication
+  app.use(
+    session({
+      secret: sessionKey,
+      resave: false,
+      saveUninitialized: false,
+    })
+  )
+
+  // Passport
   app.use(passport.initialize())
   app.use(passport.session())
 
   // Morgan
   app.use(morganLogger())
 
-  // add routes
+  // Routes
   routes.forEach((route) => app.use(`/api/${route.name}`, route.router))
 
+  // Error Handling
   app.use(errorHandler)
 
   console.info('express loaded')
