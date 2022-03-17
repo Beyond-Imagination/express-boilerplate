4c5cc216779e2dea5eb5a5f65576a5a5cf4cd7c9
laggu <laggu91@gmail.com>
2022-03-18 00:10:02 +0900

    update env loader

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import 'module-alias/register'
-import '@/loaders/env' // 이 파일 내에서 환경 변수를 사용하는 곳보다 상단에 위치해야 합니다.
+import '@/env' // 이 파일 내에서 환경 변수를 사용하는 곳보다 상단에 위치해야 합니다.
 
 import { load } from '@/loader'
 import {awsLoader, expressLoader, mongooseLoader} from '@/loaders'
-import {env} from '@/env'
+import { env } from '@/env'
 
 const { port, host } = env;
 
