36b45ea5a8f37163a500ab2256d47fbc675c0bdc
laggu <laggu91@gmail.com>
2022-03-19 10:31:53 +0900

    do not load env when NODE_ENV is test

--- src/env.ts
+++ src/env.ts
@@ -57,17 +57,18 @@ class Env {
 export let env = new Env();
 
 (() => {
+  if (process.env.NODE_ENV === "test") {
+    return
+  }
+
   const envPath = resolve(`${__dirname}/../env`)
   // development, production, test 환경 변수를 따로 관리할 경우 path를 세분화 하면 됩니다.
   const path = resolve(`${envPath}/.env`)
 
   const envFound = dotenv.config({ path })
 
-  if (process.env.NODE_ENV !== "test") {
-    if (envFound.error) {
-      console.error("couldn't find .env file")
-    } else {
-      env.load();
-    }
+  if (envFound.error) {
+    console.error("couldn't find .env file")
   }
+  env.load();
 })()
