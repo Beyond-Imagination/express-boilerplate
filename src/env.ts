220aed67019896b6bd7b9b40e706c44530f3b5de
Kuwon Sebastian Ra <sebastian.ra@groundx.xyz>
2022-03-30 00:21:32 +0900

    update env loader (#78)
    * update env loader
    * do not load env when NODE_ENV is test

--- src/env.ts
+++ src/env.ts
@@ -1,29 +1,74 @@
-import {getOsEnv, getOsEnvArray, toNumber} from '@/libs/env'
+import * as dotenv from 'dotenv'
+import { resolve } from 'path';
 
-export const env = {
-  nodeEnv: process.env.NODE_ENV || 'production',
-  isProduction: process.env.NODE_ENV === 'production',
-  isTest: process.env.NODE_ENV === 'test',
-  isDevelopment: process.env.NODE_ENV === 'development',
+import { getOsEnv, getOsEnvArray, toNumber } from '@/libs/env'
 
+class Env {
+  nodeEnv: string;
+  isProduction: boolean;
+  isTest: boolean;
+  isDevelopment: boolean;
+  
   // APPLICATION
-  port: toNumber(getOsEnv('APP_PORT', '3005')),
-  host: getOsEnv('APP_HOST', 'localhost'),
-  allowedOrigins: getOsEnvArray('ALLOWED_ORIGINS'),
-
+  port: number;
+  host: string;
+  allowedOrigins: string[];
+  
   // EXPRESS SESSION
-  sessionKey: getOsEnv('SESSION_KEY'),
-
+  sessionKey: string;
+  
   // JWT
-  jwtSecret: getOsEnv('JWT_SECRET'),
+  jwtSecret: string;
 
   // DB
-  dbURI: getOsEnv('DB_URI'),
+  dbURI: string;
 
   // AWS
   aws: {
-    profile: getOsEnv('AWS_PROFILE'),
-    region: getOsEnv('AWS_REGION', 'ap-northeast-2'),
-    s3BucketName: getOsEnv('AWS_S3_BUCKET_NAME'),
-  },
+    profile: string;
+    region: string;
+    s3BucketName: string;
+  };
+
+  load() {
+    this.nodeEnv = process.env.NODE_ENV || 'production',
+    this.isProduction = process.env.NODE_ENV === 'production',
+    this.isTest = process.env.NODE_ENV === 'test',
+    this.isDevelopment = process.env.NODE_ENV === 'development',
+  
+    this.port = toNumber(getOsEnv('APP_PORT', '3005')),
+    this.host = getOsEnv('APP_HOST', 'localhost'),
+    this.allowedOrigins = getOsEnvArray('ALLOWED_ORIGINS'),
+  
+    this.sessionKey = getOsEnv('SESSION_KEY'),
+  
+    this.jwtSecret = getOsEnv('JWT_SECRET'),
+  
+    this.dbURI = getOsEnv('DB_URI'),
+  
+    this.aws = {
+      profile: getOsEnv('AWS_PROFILE'),
+      region: getOsEnv('AWS_REGION', 'ap-northeast-2'),
+      s3BucketName: getOsEnv('AWS_S3_BUCKET_NAME'),
+    }
+  }
 }
+
+export let env = new Env();
+
+(() => {
+  if (process.env.NODE_ENV === "test") {
+    return
+  }
+
+  const envPath = resolve(`${__dirname}/../env`)
+  // development, production, test 환경 변수를 따로 관리할 경우 path를 세분화 하면 됩니다.
+  const path = resolve(`${envPath}/.env`)
+
+  const envFound = dotenv.config({ path })
+
+  if (envFound.error) {
+    console.error("couldn't find .env file")
+  }
+  env.load();
+})()
