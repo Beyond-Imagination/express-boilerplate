2b7253a2338caf086a0d3ce3bd26d01c0725a7a8
Kuwon Sebastian Ra <sebastian.ra@groundx.xyz>
2022-04-01 00:28:03 +0900

    move aws loader (#80)

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import 'module-alias/register'
 import '@/env' // 이 파일 내에서 환경 변수를 사용하는 곳보다 상단에 위치해야 합니다.
 
 import { load } from '@/loader'
-import {awsLoader, expressLoader, mongooseLoader} from '@/loaders'
+import {expressLoader, mongooseLoader} from '@/loaders'
 import { env } from '@/env'
 
 const { port, host } = env;
@@ -11,7 +11,6 @@ const { port, host } = env;
   try {
     await load()
     await mongooseLoader()
-    awsLoader()
     const app = expressLoader()
     
     app.listen(port, () => console.log(`Application running on ${host}:${port}`))
--- src/loader.ts
+++ src/loader.ts
@@ -1,5 +1,7 @@
 import { loadPassport } from '@/libs/passport'
+import { awsLoader } from '@/libs/aws'
 
 export async function load() {
     loadPassport()
+    awsLoader()
 }
